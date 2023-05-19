730530b1d845c9423855fcd3c29674bf13abe0a5
Konstantin <kdanilov1911@gmail.com>
2023-05-19 13:36:07 +0300

    first commit

---
+++ src/main/java/com/notification_telegrambot/NotificationTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.notification_telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notification_telegrambot/NotificationTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification_telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
